488f1b19365c7f966b49faf4e8e8d6565258c87a
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-05-04 01:00:55 +0300

    init + new blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/shared/components/header/header.component';
+import { HeroComponent } from './pages/shared/components/hero/hero.component';
+import { ProjectsComponent } from './pages/shared/components/projects/projects.component';
+import { LeadformComponent } from './pages/shared/components/leadform/leadform.component';
+import { BenefitsComponent } from './pages/shared/components/benefits/benefits.component';
+import { WorkflowComponent } from './pages/shared/components/workflow/workflow.component';
+import { AboutComponent } from './pages/shared/components/about/about.component';
+import { MainservicesComponent } from './pages/shared/components/mainservices/mainservices.component';
+import { ContactsComponent } from './pages/shared/components/contacts/contacts.component';
+import { FooterComponent } from './pages/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ProjectsComponent,
+    LeadformComponent,
+    BenefitsComponent,
+    WorkflowComponent,
+    AboutComponent,
+    MainservicesComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/shared/components/mainservices/mainservices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainservices',
+  templateUrl: './mainservices.component.html',
+  styleUrls: ['./mainservices.component.scss']
+})
+export class MainservicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
