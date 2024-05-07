0159cd2c68ddcc28347aac74090638ec28c4d87e
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-05-07 16:43:14 +0300

    fixes

--- src/app/pages/annex/annex.component.ts
+++ src/app/pages/annex/annex.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-annex',
   templateUrl: './annex.component.html',
-  styleUrls: ['./annex.component.scss']
+  styleUrls: ['./annex.component.scss'],
 })
 export class AnnexComponent {
-
+  gallery01 = [
+    {
+      id: 1,
+      imageSrc: 'assets/images/projects/workflowannex/1.JPG',
+    },
+    {
+      id: 2,
+      imageSrc: 'assets/images/projects/workflowannex/2.JPG',
+    },
+    {
+      id: 3,
+      imageSrc: 'assets/images/projects/workflowannex/3.JPG',
+    },
+    {
+      id: 4,
+      imageSrc: 'assets/images/projects/workflowannex/4.JPG',
+    },
+  ];
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '991px',
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 }
--- src/app/pages/shared/components/header/header.component.ts
+++ src/app/pages/shared/components/header/header.component.ts
@@ -1,15 +1,32 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private viewportscroller: ViewportScroller,
+    private router: Router
+  ) {}
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
+  ngOnInit(): void {}
+  scrollToAnchor(elementId: string): void {
+    this.router.navigateByUrl('/').then(() => {
+      // Перенаправление на главную страницу
+      this.scrollToTop(); // Прокрутить вверх страницы
+      setTimeout(() => {
+        // Добавить небольшую задержку перед прокруткой к якорю
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100); // Можно регулировать время задержки в миллисекундах
+    });
+  }
 }
