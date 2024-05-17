17c7059ac594ed9d5add2b33aacac0aada7d9701
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-05-17 17:42:47 +0300

    new fixes

--- src/app/pages/annex/annex.component.ts
+++ src/app/pages/annex/annex.component.ts
@@ -23,6 +23,122 @@ export class AnnexComponent {
       id: 4,
       imageSrc: 'assets/images/projects/workflowannex/4.JPG',
     },
+    {
+      id: 5,
+      imageSrc: 'assets/images/projects/workflowannex/5.JPG',
+    },
+    {
+      id: 6,
+      imageSrc: 'assets/images/projects/workflowannex/6.JPG',
+    },
+    {
+      id: 7,
+      imageSrc: 'assets/images/projects/workflowannex/7.JPG',
+    },
+    {
+      id: 8,
+      imageSrc: 'assets/images/projects/workflowannex/8.jpg',
+    },
+    {
+      id: 9,
+      imageSrc: 'assets/images/projects/workflowannex/9.jpg',
+    },
+    {
+      id: 10,
+      imageSrc: 'assets/images/projects/workflowannex/10.jpg',
+    },
+    {
+      id: 11,
+      imageSrc: 'assets/images/projects/workflowannex/11.jpg',
+    },
+    {
+      id: 12,
+      imageSrc: 'assets/images/projects/workflowannex/12.jpg',
+    },
+    {
+      id: 13,
+      imageSrc: 'assets/images/projects/workflowannex/13.jpg',
+    },
+    {
+      id: 14,
+      imageSrc: 'assets/images/projects/workflowannex/14.jpg',
+    },
+    {
+      id: 15,
+      imageSrc: 'assets/images/projects/workflowannex/15.jpg',
+    },
+    {
+      id: 16,
+      imageSrc: 'assets/images/projects/workflowannex/16.jpg',
+    },
+    {
+      id: 17,
+      imageSrc: 'assets/images/projects/workflowannex/17.jpg',
+    },
+    {
+      id: 18,
+      imageSrc: 'assets/images/projects/workflowannex/18.jpg',
+    },
+    {
+      id: 19,
+      imageSrc: 'assets/images/projects/workflowannex/19.jpg',
+    },
+    {
+      id: 20,
+      imageSrc: 'assets/images/projects/workflowannex/20.jpg',
+    },
+    {
+      id: 21,
+      imageSrc: 'assets/images/projects/workflowannex/21.jpg',
+    },
+    {
+      id: 22,
+      imageSrc: 'assets/images/projects/workflowannex/22.jpg',
+    },
+    {
+      id: 23,
+      imageSrc: 'assets/images/projects/workflowannex/23.jpg',
+    },
+    {
+      id: 24,
+      imageSrc: 'assets/images/projects/workflowannex/24.jpg',
+    },
+    {
+      id: 25,
+      imageSrc: 'assets/images/projects/workflowannex/25.jpg',
+    },
+    {
+      id: 26,
+      imageSrc: 'assets/images/projects/workflowannex/26.jpg',
+    },
+    {
+      id: 27,
+      imageSrc: 'assets/images/projects/workflowannex/27.jpg',
+    },
+    {
+      id: 28,
+      imageSrc: 'assets/images/projects/workflowannex/28.jpg',
+    },
+    {
+      id: 29,
+      imageSrc: 'assets/images/projects/workflowannex/29.jpg',
+    },
+    {
+      id: 30,
+      imageSrc: 'assets/images/projects/workflowannex/30.jpg',
+    },
+    {
+      id: 31,
+      imageSrc: 'assets/images/projects/workflowannex/31.jpg',
+    },
+    {
+      id: 32,
+      imageSrc: 'assets/images/projects/workflowannex/32.jpg',
+    },
+    {
+      id: 33,
+      imageSrc: 'assets/images/projects/workflowannex/33.jpg',
+    },
   ];
   responsiveOptions = [
     {
