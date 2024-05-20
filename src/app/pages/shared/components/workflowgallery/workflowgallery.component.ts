83c5eff1128635c6af2c2f87ae06c960f8a7785e
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-05-20 19:33:27 +0300

    new fixes

--- src/app/pages/shared/components/workflowgallery/workflowgallery.component.ts
+++ src/app/pages/shared/components/workflowgallery/workflowgallery.component.ts
@@ -8,20 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class WorkflowgalleryComponent implements OnInit {
   gallery01 = [
     {
-      id: 1,
-      imageSrc: 'assets/images/projects/workflowannex/1.JPG',
+      id: 8,
+      imageSrc: 'assets/images/projects/workflowannex/8.jpg',
     },
     {
-      id: 2,
-      imageSrc: 'assets/images/projects/workflowannex/2.JPG',
+      id: 17,
+      imageSrc: 'assets/images/projects/workflowannex/17.jpg',
     },
     {
-      id: 3,
-      imageSrc: 'assets/images/projects/workflowannex/3.JPG',
+      id: 22,
+      imageSrc: 'assets/images/projects/workflowannex/22.jpg',
     },
     {
-      id: 4,
-      imageSrc: 'assets/images/projects/workflowannex/4.JPG',
+      id: 23,
+      imageSrc: 'assets/images/projects/workflowannex/23.jpg',
+    },
+    {
+      id: 29,
+      imageSrc: 'assets/images/projects/workflowannex/29.jpg',
+    },
+    {
+      id: 33,
+      imageSrc: 'assets/images/projects/workflowannex/33.jpg',
     },
   ];
   gallery02 = [
@@ -46,16 +54,32 @@ export class WorkflowgalleryComponent implements OnInit {
       imageSrc: 'assets/images/projects/workflowhouses/5.JPG',
     },
     {
-      id: 6,
-      imageSrc: 'assets/images/projects/workflowhouses/6.JPG',
-    },
-    {
       id: 7,
       imageSrc: 'assets/images/projects/workflowhouses/7.JPG',
     },
     {
       id: 8,
       imageSrc: 'assets/images/projects/workflowhouses/8.JPG',
+    },
+    {
+      id: 9,
+      imageSrc: 'assets/images/projects/workflowhouses/9.jpg',
+    },
+    {
+      id: 10,
+      imageSrc: 'assets/images/projects/workflowhouses/10.jpg',
+    },
+    {
+      id: 11,
+      imageSrc: 'assets/images/projects/workflowhouses/11.jpg',
+    },
+    {
+      id: 12,
+      imageSrc: 'assets/images/projects/workflowhouses/12.jpg',
+    },
+    {
+      id: 13,
+      imageSrc: 'assets/images/projects/workflowhouses/13.jpg',
     },
   ];
   gallery03 = [
@@ -65,31 +89,63 @@ export class WorkflowgalleryComponent implements OnInit {
     },
     {
       id: 2,
-      imageSrc: 'assets/images/projects/workflowinner/4.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/2.jpg',
     },
     {
       id: 3,
-      imageSrc: 'assets/images/projects/workflowinner/5.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/3.jpg',
     },
     {
       id: 4,
-      imageSrc: 'assets/images/projects/workflowinner/7.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/4.JPG',
     },
     {
       id: 5,
-      imageSrc: 'assets/images/projects/workflowinner/9.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/5.JPG',
     },
     {
       id: 6,
-      imageSrc: 'assets/images/projects/workflowinner/10.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/6.jpg',
     },
     {
       id: 7,
-      imageSrc: 'assets/images/projects/workflowinner/12.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/7.JPG',
     },
     {
       id: 8,
-      imageSrc: 'assets/images/projects/workflowinner/18.JPG',
+      imageSrc: 'assets/images/projects/workflowinner/8.jpg',
+    },
+    {
+      id: 9,
+      imageSrc: 'assets/images/projects/workflowinner/9.JPG',
+    },
+    {
+      id: 10,
+      imageSrc: 'assets/images/projects/workflowinner/10.JPG',
+    },
+    {
+      id: 11,
+      imageSrc: 'assets/images/projects/workflowinner/11.jpg',
+    },
+    {
+      id: 12,
+      imageSrc: 'assets/images/projects/workflowinner/12.JPG',
+    },
+    {
+      id: 13,
+      imageSrc: 'assets/images/projects/workflowinner/13.jpg',
+    },
+    {
+      id: 14,
+      imageSrc: 'assets/images/projects/workflowinner/14.JPG',
+    },
+    {
+      id: 15,
+      imageSrc: 'assets/images/projects/workflowinner/15.jpg',
+    },
+    {
+      id: 16,
+      imageSrc: 'assets/images/projects/workflowinner/16.jpg',
     },
   ];
   gallery04 = [
